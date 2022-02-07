a11087145a207e6a725b4e4ff2e246b0300c5768
RodolpheBeloncle <rbeloncle@gmail.com>
2022-02-07 07:41:42 +0100

    save changes diff between axios api state and reducer state

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,25 +1,27 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import { Link } from 'react-router-dom'
-import Pokeball from '../pokeball.png'
+import React, { Component } from 'react';
+// import axios from 'axios'
+import { Link } from 'react-router-dom';
+import Pokeball from '../pokeball.png';
+import { connect } from 'react-redux';
 
 class Home extends Component {
-  state = {
-    posts: []
-  }
-  componentDidMount(){
-    axios.get('https://jsonplaceholder.typicode.com/posts/')
-      .then(res => {
-        console.log(res);
-        this.setState({
-          posts: res.data.slice(0,10)
-        });
-      })
-  }
-  render(){
-    const { posts } = this.state
+  // state = {
+  //   posts: []
+  // }
+  // componentDidMount(){
+  //   axios.get('https://jsonplaceholder.typicode.com/posts/')
+  //     .then(res => {
+  //       console.log(res);
+  //       this.setState({
+  //         posts: res.data.slice(0,10)
+  //       });
+  //     })
+  // }
+  render() {
+    console.log(this.props)
+    const { posts } = this.props;
     const postList = posts.length ? (
-      posts.map(post => {
+      posts.map((post) => {
         return (
           <div className="post card" key={post.id}>
             <img src={Pokeball} alt="A Pokeball" />
@@ -30,7 +32,7 @@ class Home extends Component {
               <p>{post.body}</p>
             </div>
           </div>
-        )
+        );
       })
     ) : (
       <div className="center">No posts to show</div>
@@ -43,8 +45,15 @@ class Home extends Component {
           {postList}
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default Home
+const mapStateToProps = (state)=> {
+  return {
+    posts: state.posts
+  }
+}
+
+// mapping posts state to Home component connected to the reducer
+export default connect(mapStateToProps)(Home);
--- poketimes/src/reducers/rootReducer.js
+++ poketimes/src/reducers/rootReducer.js
@@ -1,9 +1,14 @@
 const initState = {
-  posts: []
-}
+  posts: [
+    { id: '1', title: 'My First Title', body: 'lorem ipusm blablablal' },
+    { id: '2', title: 'My Second Title', body: 'lorem ipusm text 2' },
+    { id: '3', title: 'My Third Title', body: 'lorem ipusm text 3' },
+  ],
+};
 
 const rootReducer = (state = initState, action) => {
+  console.log(action);
   return state;
-}
+};
 
-export default rootReducer
+export default rootReducer;
